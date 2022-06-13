caf94cf1be9ac740e80cdfe7a4ecdbea866d6321
Daniel Albuquerque <99815779+DaniFCAlb@users.noreply.github.com>
2022-06-13 19:40:30 +0100

    Add files via upload

---
+++ exercicios POO/ficha 01/SomarAteAoLimite/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SomarAteAoLimite
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            int[] array = {1, 2, 3, 4, 5};
+            int sum = 0;
+
+            foreach (int num in array)
+            {
+                sum += num;
+
+            }
+            Console.WriteLine(sum);
+        }
+        
+    }
+    
+}
