6a28c80a90355832e3f03ddf4fb002e6fcb6c889
Daniel Albuquerque <99815779+DaniFCAlb@users.noreply.github.com>
2022-06-13 16:30:45 +0100

    Add files via upload

---
+++ primeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace primeNumbers
+{
+    class Program 
+    {
+        public static void Main (string[] args) 
+        {
+            
+            Console.ReadKey();
+
+        } 
+
+        public void Prime (int num)
+        {
+                bool isPrime = true;
+
+                for (int i = 2; i <= num; i++)  
+                {
+                    for (int j = 0; j <= num; j++)
+                    {
+                        if (i != j && i% j== 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime)
+                    {
+                        Console.WriteLine("prime" + i);
+                    }
+                    isPrime = true;
+                }
+        }
+    }
+}
