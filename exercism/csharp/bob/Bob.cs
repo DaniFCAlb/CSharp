e405286de1fbc2c4c436c9235386b763c8113f3c
Daniel Albuquerque <danielalbuquerque16@gmail.com>
2022-08-01 15:35:08 +0100

    chamar metdos no switch

--- exercism/csharp/bob/Bob.cs
+++ exercism/csharp/bob/Bob.cs
@@ -4,6 +4,40 @@ public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+      switch (statement)
+      {
+        case Question():
+            return "Sure";
+            break;
+        
+        default:
+            return "Whatever";
+      }
+    
+      
     }
+    public static bool Question(string statement)
+    {
+        return statement.Contains("?");
+    }
+
+    static bool Yell(string statement)
+    {
+        return statement == statement.ToUpper() 
+                && statement != statement.ToLower(); 
+    }
+
+    static bool YellQuestion(string statement)
+    {
+        return statement == statement.ToUpper() 
+                && statement != statement.ToLower() 
+                && statement.Contains("?"); 
+    }
+
+    static bool SayNothing(string statement)
+    {
+        return string.IsNullOrEmpty("");
+    }
+    
+    
 }
