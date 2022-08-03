ab1acaf30f3e537d480b93bb6c35f6dd4f71a7b9
DaniFCAlb <danielalbuquerque16@gmail.com>
2022-08-03 19:14:52 +0100

    git test

--- exercism/csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ exercism/csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,24 +5,16 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        
-
-      for (int a = 1; a < sum/3 ; a++)
-      {
-        for (int b = a + 1; b < sum/2; b++)
-        {             
+      for (int a = 1; a < sum/3 ; a++) {
+        for (int b = a + 1; b < sum/2; b++) {             
           int c = sum -a -b;
                 
-            if (a*a + b*b == c*c)
-            {
+            if (a*a + b*b == c*c) {
               yield return (a, b, c); // (colher / produto) retorna os valores do tuple, 1 de cada vez
             }
             
-            
+            // tetstar git 
         }
-    
       }    
-      
-      
     }
 }
