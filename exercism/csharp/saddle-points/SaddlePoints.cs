c0b482b0a27bb4b2cd802670b55aabc522247193
Daniel Albuquerque <danielalbuquerque16@gmail.com>
2022-08-08 15:02:22 +0100

    descobrir index ponto de sela

---
+++ exercism/csharp/saddle-points/SaddlePoints.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SaddlePoints {
+    
+    public static IEnumerable<(int, int)> Calculate(int[,] matrix) {
+
+       // iterar por toda a matriz, linha a linha
+       // descobrir o max da linha atual 
+       // verificar se este max também é o min da coluna 
+       // verificar se há 0 ou + pontos sela 
+       
+    }
+}
