14b4553a7c631c4d10e15f6d00efd2210563bab7
Daniel Albuquerque <danielalbuquerque16@gmail.com>
2022-08-08 17:00:35 +0100

    primeiro loop

--- exercism/csharp/saddle-points/SaddlePoints.cs
+++ exercism/csharp/saddle-points/SaddlePoints.cs
@@ -10,6 +10,16 @@ public static class SaddlePoints {
        // descobrir o max da linha atual 
        // verificar se este max também é o min da coluna 
        // verificar se há 0 ou + pontos sela 
-       
+        
+        for (int i = 0; i < matrix.Length; i++) { 
+            
+            for (int j = 0; j < matrix.Length; j++) {
+                
+                if (i == j) {
+                   yield return (i, j);   
+                   
+                }
+            }
+        }
     }
 }
