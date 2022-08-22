b960087572afb6590e32a5115b68a8eaae91e91d
Daniel Albuquerque <danielalbuquerque16@gmail.com>
2022-08-22 14:48:50 +0100

    same doubt

--- exercism/csharp/saddle-points/SaddlePoints.cs
+++ exercism/csharp/saddle-points/SaddlePoints.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 
 public static class SaddlePoints {
-    
     public static IEnumerable<(int, int)> Calculate(int[,] matrix) {
 
        // iterar por toda a matriz, linha a linha
