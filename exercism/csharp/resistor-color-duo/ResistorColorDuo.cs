b103ee92712c669f8b3b73e774bb64b7e31d0102
Daniel Albuquerque <danielalbuquerque16@gmail.com>
2022-09-01 18:24:23 +0100

    condição da iteração correta

--- exercism/csharp/resistor-color-duo/ResistorColorDuo.cs
+++ exercism/csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -2,12 +2,13 @@ using System;
 
 public static class ResistorColorDuo {
     public static int Value(string[] colors) {
-        string[] color = {"black", "brown", "red", "orange", "yellow", "green",
+        string[] bandColors = {"black", "brown", "red", "orange", "yellow", "green",
                                         "blue", "violet", "grey", "white"};
         string resist = "";
-        for (int i = 0; i < color.Length; i++) { // index fora do limite do array i < color.Length
+
+        for (int i = 0; i < colors.Length; i++) { // index fora do limite do array i < color.Length
             //resist += Array.IndexOf(colors, color[i]); mal....
-            resist += Array.IndexOf(color, colors[i]);
+            resist += Array.IndexOf(bandColors, colors[i]);
             Console.WriteLine(resist);
         }     
         return Int32.Parse(resist); // converter string para int
