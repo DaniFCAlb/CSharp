04ea449724b2bfa9a2ff8fe2e7141a290f1e2f02
Daniel Albuquerque <danielalbuquerque16@gmail.com>
2022-09-02 10:13:42 +0100

    for loop correto

--- exercism/csharp/resistor-color-duo/ResistorColorDuo.cs
+++ exercism/csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -5,11 +5,10 @@ public static class ResistorColorDuo {
         string[] bandColors = {"black", "brown", "red", "orange", "yellow", "green",
                                         "blue", "violet", "grey", "white"};
         string resist = "";
-
-        for (int i = 0; i < colors.Length; i++) { // index fora do limite do array i < color.Length
+        for (int i = 0; i < 2; i++) { 
             //resist += Array.IndexOf(colors, color[i]); mal....
             resist += Array.IndexOf(bandColors, colors[i]);
-            Console.WriteLine(resist);
+            Console.WriteLine(resist);    
         }     
         return Int32.Parse(resist); // converter string para int
     }
